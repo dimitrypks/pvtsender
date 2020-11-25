479fe76ae3c7c6960e480b60f4d1f68ef7f4ea9e
zlayine <zlayine@1337.student.ma>
2020-11-25 14:59:09 +0100

    test

--- install.sh
+++ install.sh
@@ -18,31 +18,31 @@ echo -e "\033[1;32mGenerating app key..\033[0m"
 
 docker-compose exec laravel-app php artisan key:generate 
 
-echo -e "\033[1;32mGenerating the database..\033[0m"
+# echo -e "\033[1;32mGenerating the database..\033[0m"
 
-docker-compose exec laravel-app php artisan migrate 
+# docker-compose exec laravel-app php artisan migrate 
 
-echo -e "\033[1;32mGenerating auth secret..\033[0m"
+# echo -e "\033[1;32mGenerating auth secret..\033[0m"
 
-docker-compose exec laravel-app php artisan jwt:secret
+# docker-compose exec laravel-app php artisan jwt:secret
 
-echo -e "\033[1;32mSeeding the database..\033[0m"
+# echo -e "\033[1;32mSeeding the database..\033[0m"
 
-docker-compose exec laravel-app php artisan db:seed
+# docker-compose exec laravel-app php artisan db:seed
 
-echo -e "\033[1;32mLinking storage..\033[0m"
+# echo -e "\033[1;32mLinking storage..\033[0m"
 
-docker-compose exec laravel-app php artisan storage:link
+# docker-compose exec laravel-app php artisan storage:link
 
-chmod -R 777 storage
+# chmod -R 777 storage
 
-echo -e "\033[1;32mStarting supervisor..\033[0m"
+# echo -e "\033[1;32mStarting supervisor..\033[0m"
 
-docker-compose exec laravel-app service supervisor start
+# docker-compose exec laravel-app service supervisor start
 
-# clear
-echo -e "\033[1;32mInstallation complete..Happy sending!\033[0m"
+# # clear
+# echo -e "\033[1;32mInstallation complete..Happy sending!\033[0m"
 
-echo "APPLINK: http://$APP_SERVER/s3ndpvt2020"
-echo "Phpmyadmin: http://$APP_SERVER:7000/    root:h3eShd12sa"
+# echo "APPLINK: http://$APP_SERVER/s3ndpvt2020"
+# echo "Phpmyadmin: http://$APP_SERVER:7000/    root:h3eShd12sa"
 
