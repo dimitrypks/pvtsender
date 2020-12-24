c7cddb6b243570357210eeb459965bf6b08ad5b2
zlayine <zlayine@1337.student.ma>
2020-12-24 12:59:06 +0100

    fix

--- install.sh
+++ install.sh
@@ -4,52 +4,51 @@ cp .env.example .env
 
 echo -e "\033[1;32mBuilding the docker environment..Will take 5 minutes\033[0m"
 
-docker-compose build 
+docker-compose build &> /dev/null
 
 echo -e "\033[1;32mStarting the services..\033[0m"
 
-docker-compose up -d 
+docker-compose up -d &> /dev/null
 
 echo -e "\033[1;32mBuilding the depencies..\033[0m"
 
-docker-compose exec laravel-app composer install 
+docker-compose exec laravel-app composer install &> /dev/null
 
 echo -e "\033[1;32mGenerating app key..\033[0m"
 
-docker-compose exec laravel-app php artisan key:generate 
+docker-compose exec laravel-app php artisan key:generate &> /dev/null
 
 \cp -fR phpmailer vendor/
 
 echo -e "\033[1;32mGenerating the database..\033[0m"
 
-docker-compose down 
+docker-compose down &> /dev/null
 
-docker-compose up -d 
+docker-compose up -d &> /dev/null
 
 sleep 10
 
-docker-compose exec laravel-app php artisan migrate 
+docker-compose exec laravel-app php artisan migrate &> /dev/null
 
 echo -e "\033[1;32mGenerating auth secret..\033[0m"
 
-docker-compose exec laravel-app php artisan jwt:secret 
+docker-compose exec laravel-app php artisan jwt:secret &> /dev/null
 
 echo -e "\033[1;32mSeeding the database..\033[0m"
 
-docker-compose exec laravel-app php artisan db:seed 
+docker-compose exec laravel-app php artisan db:seed &> /dev/null
 
 echo -e "\033[1;32mLinking storage..\033[0m"
 
-docker-compose exec laravel-app php artisan storage:link 
+docker-compose exec laravel-app php artisan storage:link &> /dev/null
 
 chmod -R 777 storage
 chmod 666 .env
 touch storage/app/public/debug.txt
 chmod 666 storage/app/public/debug.txt
-touch 
 echo -e "\033[1;32mStarting supervisor..\033[0m"
 
-docker-compose exec laravel-app service supervisor start 
+docker-compose exec laravel-app service supervisor start &> /dev/null
 
 clear
 echo -e "\033[1;32mInstallation complete..Happy sending!\033[0m"
